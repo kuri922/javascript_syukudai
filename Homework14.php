64feb4329218fcfd7819334529cfb64b09ad8d24
栗山雅史 <kuriyama@masafuminoMacBook-Air.local>
2021-09-28 18:22:35 +0900

    php

--- Homework14.php
+++ Homework14.php
@@ -4,30 +4,29 @@
 
 
 
-function check($num1,$num2){
+    function check($num1,$num2){
 
-  //両方とも７５点以上の場合合格
+    //両方とも７５点以上の場合合格
 
-    if($num1 > 75 and $num2 > 75){    
-        echo("Pass");
+            if($num1 > 75 and $num2 > 75){    
+             echo("Pass");
 
     //合計が１４０点以上の場合合格
 
-    }elseif($num1+$num2 >=140) {
-        echo("Pass");
+            }elseif($num1+$num2 >140) {
+            echo("Pass");
 
     //合計が１００点以上で、どちらかが９０点以上あれば合格
 
-    }elseif($num1+$num2 >=100&&$num1 or $num2 >= 90){
-        echo("Pass");
-
+            }elseif($num1+$num2 >=100 && ($num1 > 90 or $num2 > 90)){
+            echo("Pass");
+     
     //どれにも該当しない
 
-     }else {
-        echo("Fail");
-     }
- }
- 
- check(75,90);
+            }else{
+             echo("Fail");
+            }
+    }
+    check(10,85);
 
 ?>
